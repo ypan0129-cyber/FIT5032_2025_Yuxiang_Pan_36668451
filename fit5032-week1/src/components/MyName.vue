3dc32804716e47638be11525bb614e4b23ea970e
qq_56039130 <pyx_15755142918@163.com>
2026-07-02 16:48:50 +0800

    Submit week1 and week2

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="my-name">
+    <h2>Hello, My Name is Yuxiang Pan！</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+.my-name {
+  color: #42b983;
+  margin-top: 20px;
+}
+</style>
---
+++ fit5032-week2/ypan-library/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// 引入你写好的 JSON 组件
+import HelloWorld from './components/JSON.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
+
+<style scoped>
+/* 这里可以暂时留空 */
+</style>
