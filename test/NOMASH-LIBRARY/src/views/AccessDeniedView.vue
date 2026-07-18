fa8cfae8241dfc59b633a0cde6de7ad52f48090a
ypan0129-cyber <ypan0129@student.monash.edu>
2026-07-18 12:01:56 +0800

    Delete test folder

--- test/NOMASH-LIBRARY/src/views/AccessDeniedView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container text-center mt-5">
-    <h1 class="text-danger">Access Denied</h1>
-    <p class="lead">You do not have permission to view this page. Please login first.</p>
-    <router-link to="/login" class="btn btn-primary">Go to Login</router-link>
-  </div>
-</template>
