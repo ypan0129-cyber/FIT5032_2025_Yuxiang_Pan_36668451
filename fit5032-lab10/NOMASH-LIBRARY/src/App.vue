198badbd72f72077a3628232cc4276dc76427576
ypan0129-cyber <ypan0129@student.monash.edu>
2026-08-07 11:48:41 +0800

    fit5032-lab10

---
+++ fit5032-lab10/NOMASH-LIBRARY/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import BHeader from './components/BHeader.vue'
+// import LibraryRegistrationForm from './views/HomeView.vue/index.js'
+</script>
+
+<template>
+  <BHeader />
+
+  <main>
+    <router-view></router-view>
+    <!-- <JSONLab /> -->
+  </main>
+</template>
+
+<style>
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 80vw;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+
+.list-group-item {
+  padding: 10px;
+}
+</style>
